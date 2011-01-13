cbe6bcae5f1c899d7e7e0baa3ce3d7ea90542660
al3xeng <alexcristianeng@gmail.com>
2011-01-13 20:03:32 -0200

    Estrutura básica de um projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+College::Application.config.secret_token = 'ee5fe88fe53e11ca15475c777099c81d3fb338627448502c87b2385f3fff94e863b5326a0f2aa31b13ff4eb675e0eabfd444a00c46118cfc4e96fc6d671e0cfa'
